50e6b2d0423914b5f1bc108115baf4b89e51f36c
Nigel-Kallan <nigel.kallan@triosstudent.com>
2026-07-30 19:12:33 -0400

    Initial Commit

---
+++ iOSApp3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSApp3
+//
+//  Created by NIGEL KALLAN on 2026-07-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSApp3/iOSApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp3App.swift
+//  iOSApp3
+//
+//  Created by NIGEL KALLAN on 2026-07-30.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
